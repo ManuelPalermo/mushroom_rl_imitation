d70be5b2af4f77acc20c719e28ff5244379efc8d
Palermo <macpalermo@gmail.com>
2020-03-18 20:21:05 +0000

    Implemented versions of GAIL and VAIL which use TRPO policy updates instead of PPO

---
+++ ImitationLearning/vail_TRPO.py
@@ -0,0 +1,247 @@
+from copy import deepcopy
+
+import numpy as np
+
+import torch
+from mushroom_rl.algorithms.actor_critic import TRPO
+from torch import nn
+from torch.nn.modules.loss import BCELoss
+import torch.nn.functional as F
+
+from mushroom_rl.approximators import Regressor
+from mushroom_rl.approximators.parametric import TorchApproximator
+from mushroom_rl.utils.minibatches import minibatch_generator
+from mushroom_rl.utils.torch import get_gradient, zero_grad, to_float_tensor
+from mushroom_rl.utils.dataset import parse_dataset
+from mushroom_rl.utils.value_functions import compute_gae
+
+
+class VDBnetwork(nn.Module):
+    """
+    Variational Discriminator Bottleneck network.
+
+    "Variational Discriminator Bottleneck: Improving Imitation Learning,
+        Inverse RL, and GANs by Constraining Information Flow"
+    Peng X. et al.. 2019.
+
+    """
+    def __init__(self, input_shape, output_shape, n_features, z_size, **kwargs):
+        super(VDBnetwork, self).__init__()
+
+        n_input = input_shape[-1]
+        n_output = output_shape[-1] # should always be 1(just like this for consistency)
+
+        # encoder
+        self._in = nn.Linear(n_input, n_features)
+        self._z_mu = nn.Linear(n_features, z_size)
+        self._z_v = nn.Linear(n_features, z_size)
+
+        # decoder
+        self._h1 = nn.Linear(z_size, n_features)
+        self._out = nn.Linear(n_features, n_output)
+
+    def encoder(self, x):
+        h = torch.tanh(self._in(x))
+        return self._z_mu(h), self._z_v(h)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(logvar / 2)
+        eps = torch.randn_like(std)
+        return mu + std * eps
+
+    def discriminator(self, z):
+        h = torch.tanh(self._h1(z))
+        return torch.sigmoid(self._out(h))
+
+    def forward(self, x, **kwargs):
+        mu, logvar = self.encoder(x)
+        z = self.reparameterize(mu, logvar)
+        prob = self.discriminator(z)
+        return prob, mu, logvar
+
+
+class VDBloss(BCELoss):
+    """
+    Variational Discriminator Bottleneck loss.
+
+    "Variational Discriminator Bottleneck: Improving Imitation Learning,
+        Inverse RL, and GANs by Constraining Information Flow"
+    Peng X. et al.. 2019.
+
+    """
+    __constants__ = ['reduction', 'weight']
+
+    def __init__(self, info_constraint, lr_beta,
+                 weight=None, size_average=None, reduce=None, reduction='mean'):
+        super(VDBloss, self).__init__(weight, size_average, reduce, reduction)
+
+        self._info_constr = info_constraint
+        self._lr_beta = lr_beta
+        self._beta = 0
+
+    def forward(self, inputs, target):
+        prob, mu, logvar = inputs
+
+        # bottleneck loss
+        kld = kl_divergence(mu, logvar).mean()
+        bottleneck_loss = kld - self._info_constr
+        self._beta = max(0, self._beta + self._lr_beta * bottleneck_loss)
+
+        # calculate discriminator loss(BinaryCrossEntropy + bottleneck)
+        vdb_loss = (F.binary_cross_entropy(prob, target, weight=self.weight, reduction=self.reduction)
+                    + self._beta * bottleneck_loss)
+        return vdb_loss
+
+
+def kl_divergence(mu, logvar):
+    kl_div = 0.5 * torch.sum(mu.pow(2) + logvar.exp() - logvar - 1, dim=1)
+    return kl_div
+
+
+class TorchApproximator_(TorchApproximator):
+    def _fit_batch(self, batch, use_weights, kwargs):
+        loss = self._compute_batch_loss(batch, use_weights, kwargs)
+
+        # needs to retain graph for the dual gradient descent(beta)
+        self._optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        self._optimizer.step()
+
+        return loss.item()
+
+
+class VAIL(TRPO):
+    """
+    Variational Adversarial Imitation Learning(VAIL) implementation
+
+    "Variational Discriminator Bottleneck: Improving Imitation Learning,
+        Inverse RL, and GANs by Constraining Information Flow"
+    Peng X. et al.. 2019.
+
+    """
+
+    def __init__(self, mdp_info, policy_class, policy_params,
+                 discriminator_params, critic_params,
+                 ent_coeff=0., max_kl=.001, lam=1.,
+                 n_epochs_line_search=10, n_epochs_cg=10,
+                 cg_damping=1e-2, cg_residual_tol=1e-10,
+                 demonstrations=None, info_constraint=0.5, lr_beta=1e-4,
+                 env_reward_frac=0.0, state_mask=None, act_mask=None, quiet=True,
+                 critic_fit_params=None, discriminator_fit_params=None):
+
+        # initialize PPO agent
+        policy = policy_class(**policy_params)
+        super(VAIL, self).__init__(mdp_info, policy, critic_params,
+                                   ent_coeff, max_kl, lam, n_epochs_line_search,
+                                   n_epochs_cg, cg_damping, cg_residual_tol,
+                                   quiet=quiet, critic_fit_params=critic_fit_params)
+
+        # discriminator params
+        self._discriminator_fit_params = (dict(n_epochs=1) if discriminator_fit_params is None
+                                          else discriminator_fit_params)
+
+        discriminator_params["network"] = VDBnetwork
+        discriminator_params["loss"] = VDBloss(info_constraint, lr_beta)
+        discriminator_params.setdefault("z_size", 8)
+        discriminator_params.setdefault("batch_size", 128)
+        self._D = Regressor(TorchApproximator_, **discriminator_params)
+
+        self._env_reward_frac = env_reward_frac
+        self._demonstrations = demonstrations   # should be: dict(states=np.array, actions=(np.array/None))
+        assert 0.0 <= env_reward_frac <= 1.0, "Environment reward must be between [0,1]"
+        assert demonstrations is not None or env_reward_frac == 1.0, "No demonstrations have been loaded"
+
+        # select which observations / actions to discriminate
+        if not "actions" in demonstrations:
+            act_mask = []
+
+        self._state_mask = np.arange(demonstrations["states"].shape[1]) \
+            if state_mask is None else state_mask
+
+        self._act_mask = np.arange(demonstrations["actions"].shape[1]) \
+            if act_mask is None else act_mask
+
+    def load_demonstrations(self, demonstrations):
+        self._demonstrations = demonstrations
+
+    def fit(self, dataset):
+        state, action, reward, next_state, absorbing, last = parse_dataset(dataset)
+        x = state.astype(np.float32)
+        u = action.astype(np.float32)
+        r = reward.astype(np.float32)
+        xn = next_state.astype(np.float32)
+
+        obs = to_float_tensor(x, self.policy.use_cuda)
+        act = to_float_tensor(u, self.policy.use_cuda)
+
+        if self._env_reward_frac < 1.0:
+            # fit discriminator
+            self._fit_discriminator(x, u)
+
+            # create reward from the discriminator(can use fraction of environment reward)
+            r_disc = self._make_discrim_reward(x, u)
+            r = r * self._env_reward_frac + r_disc * (1 - self._env_reward_frac)
+
+        v_target, np_adv = compute_gae(self._V, x, xn, r, absorbing, last,
+                                       self.mdp_info.gamma, self._lambda)
+        np_adv = (np_adv - np.mean(np_adv)) / (np.std(np_adv) + 1e-8)
+        adv = to_float_tensor(np_adv, self.policy.use_cuda)
+
+        # Policy update
+        self._old_policy = deepcopy(self.policy)
+        old_pol_dist = self._old_policy.distribution_t(obs)
+        old_log_prob = self._old_policy.log_prob_t(obs, act).detach()
+
+        zero_grad(self.policy.parameters())
+        loss = self._compute_loss(obs, act, adv, old_log_prob)
+
+        prev_loss = loss.item()
+
+        # Compute Gradient
+        loss.backward()
+        g = get_gradient(self.policy.parameters())
+
+        # Compute direction through conjugate gradient
+        stepdir = self._conjugate_gradient(g, obs, old_pol_dist)
+
+        # Line search
+        self._line_search(obs, act, adv, old_log_prob, old_pol_dist, prev_loss, stepdir)
+
+        # VF update
+        self._V.fit(x, v_target, **self._critic_fit_params)
+
+        # Print fit information
+        self._print_fit_info(dataset, x, v_target, old_pol_dist)
+        self._iter += 1
+
+    def _fit_discriminator(self, plcy_obs, plcy_act):
+        plcy_obs = plcy_obs[:, self._state_mask]
+        plcy_act = plcy_act[:, self._act_mask]
+
+        # get batch of data to discriminate
+        if not self._act_mask:
+            demo_obs = next(minibatch_generator(plcy_obs.shape[0],
+                                                self._demonstrations["states"]))[0]
+            inputs = np.concatenate([plcy_obs, demo_obs.astype(np.float32)])
+        else:
+            demo_obs, demo_act = next(minibatch_generator(plcy_obs.shape[0],
+                                                          self._demonstrations["states"],
+                                                          self._demonstrations["actions"]))
+            plcy_data = np.concatenate([plcy_obs, plcy_act], axis=1)
+            demo_data = np.concatenate([demo_obs, demo_act], axis=1)
+            inputs = np.concatenate([plcy_data, demo_data.astype(np.float32)])
+
+        # create label targets: (demos(~1) or policy(~0))
+        plcy_target = np.zeros((plcy_obs.shape[0], 1), dtype=np.float32)
+        demo_target = np.ones((demo_obs.shape[0], 1), dtype=np.float32)
+        targets = np.concatenate([plcy_target, demo_target])
+
+        self._D._impl.model._loss._beta = 0  # set dual beta=0
+        self._D.fit(inputs, targets, **self._discriminator_fit_params)
+
+    def _make_discrim_reward(self, state, action):
+        plcy_data = np.concatenate([state[:, self._state_mask],
+                                    action[:, self._act_mask]], axis=1)
+        with torch.no_grad():
+            plcy_prob = self._D(plcy_data)[0]
+            return np.squeeze(-np.log(1 - plcy_prob + 1e-8)).astype(np.float32)
---
+++ examples/example_gail_TRPO_pendulum.py
@@ -0,0 +1,350 @@
+from copy import deepcopy
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from mushroom_rl.utils.preprocessors import NormalizationBoxedPreprocessor
+
+from mushroom_rl.policy import GaussianTorchPolicy
+from mushroom_rl.environments import Gym
+from mushroom_rl.core import Core
+from mushroom_rl.utils.dataset import compute_J
+
+from ImitationLearning.vail_TRPO import VAIL
+from ImitationLearning.gail_TRPO import GAIL
+
+
+class CriticNetwork(nn.Module):
+    # For sac agent
+    def __init__(self, input_shape, output_shape, n_features, **kwargs):
+        super().__init__()
+
+        n_input = input_shape[-1]
+        n_output = output_shape[0]
+
+        self._h1 = nn.Linear(n_input, n_features[0])
+        self._h2 = nn.Linear(n_features[0], n_features[1])
+        self._h3 = nn.Linear(n_features[1], n_output)
+
+        nn.init.xavier_uniform_(self._h1.weight,
+                                gain=nn.init.calculate_gain('relu'))
+        nn.init.xavier_uniform_(self._h2.weight,
+                                gain=nn.init.calculate_gain('relu'))
+        nn.init.xavier_uniform_(self._h3.weight,
+                                gain=nn.init.calculate_gain('linear'))
+
+    def forward(self, state, action):
+        state_action = torch.cat((state.float(), action.float()), dim=1)
+        features1 = F.relu(self._h1(state_action))
+        features2 = F.relu(self._h2(features1))
+        q = self._h3(features2)
+        return torch.squeeze(q)
+
+
+class ActorNetwork(nn.Module):
+    def __init__(self, input_shape, output_shape, n_features, **kwargs):
+        super(ActorNetwork, self).__init__()
+
+        n_input = input_shape[-1]
+        n_output = output_shape[0]
+
+        self._in = nn.Linear(n_input, n_features[0])
+        self._h1 = nn.Linear(n_features[0], n_features[1])
+        self._out = nn.Linear(n_features[1], n_output)
+
+        nn.init.xavier_uniform_(self._in.weight,
+                                gain=nn.init.calculate_gain('leaky_relu'))
+        nn.init.xavier_uniform_(self._h1.weight,
+                                gain=nn.init.calculate_gain('leaky_relu'))
+        nn.init.xavier_uniform_(self._out.weight,
+                                gain=nn.init.calculate_gain('linear'))
+
+    def forward(self, x, **kwargs):
+        x = torch.squeeze(x, 1).float()
+        x = F.leaky_relu(self._in(x), 0.2)
+        x = F.leaky_relu(self._h1(x), 0.2)
+        out = self._out(x)
+        return out
+
+
+class DiscriminatorNetwork(nn.Module):
+    def __init__(self, input_shape, output_shape, n_features, **kwargs):
+        super(DiscriminatorNetwork, self).__init__()
+
+        n_input = input_shape[-1]
+        n_output = output_shape[0]
+
+        self._in = nn.Linear(n_input, n_features[0])
+        self._h1 = nn.Linear(n_features[0], n_features[1])
+        self._out = nn.Linear(n_features[1], n_output)
+
+        nn.init.xavier_uniform_(self._in.weight,
+                                gain=nn.init.calculate_gain('leaky_relu'))
+        nn.init.xavier_uniform_(self._h1.weight,
+                                gain=nn.init.calculate_gain('leaky_relu'))
+        nn.init.xavier_uniform_(self._out.weight,
+                                gain=nn.init.calculate_gain('sigmoid'))
+
+    def forward(self, x, **kwargs):
+        x = F.leaky_relu(self._in(x), 0.2)
+        x = F.leaky_relu(self._h1(x), 0.2)
+        out = torch.sigmoid(self._out(x))
+        return out
+
+
+def _create_gail_agent(mdp, disc_only_state=False, **kwargs):
+    use_cuda = False    # torch.cuda.is_available()
+
+    mdp_info = deepcopy(mdp.info)
+
+    # Settings
+    network_layers_actor = (128, 64)
+    network_layers_critic = (128, 64)
+    network_layers_discriminator = (128, 64)
+
+    lr_critic = 3e-4
+    lr_discriminator = 3e-4
+
+    weight_decay_critic = 0.0
+    weight_decay_discriminator = 0.0
+
+    gae_lambda = .95
+    policy_std_0 = 0.3
+
+    batch_size_discriminator = 128
+
+    discrim_obs_mask = np.arange(mdp_info.observation_space.shape[0])
+    discrim_act_mask = [] if disc_only_state else np.arange(mdp_info.action_space.shape[0])
+    discrim_input_shape = (len(discrim_obs_mask) + len(discrim_act_mask),)
+
+    policy_params = dict(network=ActorNetwork,
+                         input_shape=mdp_info.observation_space.shape,
+                         output_shape=mdp_info.action_space.shape,
+                         std_0=policy_std_0,
+                         n_features=network_layers_actor,
+                         use_cuda=use_cuda,
+                         )
+
+    critic_params = dict(network=ActorNetwork,
+                         optimizer={'class':  optim.Adam,
+                                    'params': {'lr':           lr_critic,
+                                               'weight_decay': weight_decay_critic}},
+                         loss=F.mse_loss,
+                         input_shape=mdp_info.observation_space.shape,
+                         output_shape=(1,),
+                         n_features=network_layers_critic,
+                         use_cuda=use_cuda,
+                         )
+
+    discriminator_params = dict(optimizer={'class':  optim.Adam,
+                                           'params': {'lr':           lr_discriminator,
+                                                      'weight_decay': weight_decay_discriminator}},
+                                batch_size=batch_size_discriminator,
+
+                                network=DiscriminatorNetwork,
+                                input_shape=discrim_input_shape,
+                                n_features=network_layers_discriminator,
+                                use_cuda=use_cuda,
+                                )
+
+    alg_params = dict(ent_coeff=0.,
+                      max_kl=.001,
+                      lam=gae_lambda,
+                      n_epochs_line_search=10,
+                      n_epochs_cg=10,
+                      cg_damping=1e-2,
+                      cg_residual_tol=1e-10,
+                      state_mask=discrim_obs_mask,
+                      act_mask=discrim_act_mask,
+                      quiet=True
+                      )
+
+    # TorchApproximator parameters (used for behaviour cloning)
+    torch_approx_params = dict(batch_size=128,
+                               optimizer={'class':  optim.Adam,
+                                          'params': {'lr':           1e-3,
+                                                     'weight_decay': 1e-5}},
+                               loss=torch.nn.MSELoss(),
+                               )
+    policy_params = {**policy_params, **torch_approx_params}
+
+    # load expert training data
+    expert_files = np.load("expert_data/expert_dataset_pendulum_SAC_120.npz")
+    inputs = expert_files["obs"][:200*10]
+    outputs = expert_files["actions"][:200*10]
+    demonstrations = dict(states=inputs, actions=outputs)
+
+    agent = GAIL(mdp_info=mdp_info, policy_class=GaussianTorchPolicy, policy_params=policy_params,
+                 discriminator_params=discriminator_params, critic_params=critic_params,
+                 demonstrations=demonstrations, **alg_params)
+    return agent
+
+
+def _create_vail_agent(mdp, disc_only_state=False, **kwargs):
+    use_cuda = False    # torch.cuda.is_available()
+
+    mdp_info = deepcopy(mdp.info)
+
+    # Settings
+    network_layers_actor = (128, 64)
+    network_layers_critic = (128, 64)
+    network_layers_discriminator = 128
+
+    lr_critic = 3e-4
+    lr_discriminator = 3e-4
+
+    weight_decay_critic = 0.0
+    weight_decay_discriminator = 0.0
+
+    gae_lambda = .95
+    policy_std_0 = 0.3
+
+    d_noise_vector_size = 8
+    batch_size_discriminator = 128
+    info_constraint = 0.5
+    lr_beta = 3e-5
+
+    discrim_obs_mask = np.arange(mdp_info.observation_space.shape[0])
+    discrim_act_mask = [] if disc_only_state else np.arange(mdp_info.action_space.shape[0])
+    discrim_input_shape = (len(discrim_obs_mask) + len(discrim_act_mask),)
+
+    policy_params = dict(network=ActorNetwork,
+                         input_shape=mdp_info.observation_space.shape,
+                         output_shape=mdp_info.action_space.shape,
+                         std_0=policy_std_0,
+                         n_features=network_layers_actor,
+                         use_cuda=use_cuda,
+                         )
+
+    critic_params = dict(network=ActorNetwork,
+                         optimizer={'class':  optim.Adam,
+                                    'params': {'lr':           lr_critic,
+                                               'weight_decay': weight_decay_critic}},
+                         loss=F.mse_loss,
+                         input_shape=mdp_info.observation_space.shape,
+                         output_shape=(1,),
+                         n_features=network_layers_critic,
+                         use_cuda=use_cuda,
+                         )
+
+    discriminator_params = dict(optimizer={'class':  optim.Adam,
+                                           'params': {'lr':           lr_discriminator,
+                                                      'weight_decay': weight_decay_discriminator}},
+                                batch_size=batch_size_discriminator,
+                                z_size=d_noise_vector_size,
+
+                                input_shape=discrim_input_shape,
+                                output_shape=(1,),
+                                n_features=network_layers_discriminator,
+                                use_cuda=use_cuda,
+                                )
+
+    alg_params = dict(ent_coeff=0.,
+                      max_kl=.001,
+                      lam=gae_lambda,
+                      n_epochs_line_search=10,
+                      n_epochs_cg=10,
+                      cg_damping=1e-2,
+                      cg_residual_tol=1e-10,
+                      info_constraint=info_constraint,
+                      lr_beta=lr_beta,
+                      state_mask=discrim_obs_mask,
+                      act_mask=discrim_act_mask,
+                      quiet=True
+                      )
+
+    # TorchApproximator parameters (used for behaviour cloning)
+    torch_approx_params = dict(batch_size=128,
+                               optimizer={'class':  optim.Adam,
+                                          'params': {'lr':           1e-3,
+                                                     'weight_decay': 1e-5}},
+                               loss=torch.nn.MSELoss(),
+                               )
+    policy_params = {**policy_params, **torch_approx_params}
+
+    # load expert training data
+    expert_files = np.load("expert_data/expert_dataset_pendulum_SAC_120.npz")
+    inputs = expert_files["obs"][:200*10]
+    outputs = expert_files["actions"][:200*10]
+    demonstrations = (dict(states=inputs, actions=outputs))
+
+    agent = VAIL(mdp_info=mdp_info, policy_class=GaussianTorchPolicy, policy_params=policy_params,
+                 discriminator_params=discriminator_params, critic_params=critic_params,
+                 demonstrations=demonstrations, **alg_params)
+    return agent
+
+
+def init_policy_with_bc(agent, normalizer=None):
+    if normalizer is None:
+        normalizer = lambda x: x
+
+    # load expert training data
+    expert_files = np.load("expert_data/expert_dataset_pendulum_SAC_120.npz")
+    inputs = normalizer(expert_files["obs"])[:200*10]
+    outputs = expert_files["actions"][:200*10]
+
+    # initialize policy mu network through behaviour cloning
+    agent.policy._mu.fit(inputs, outputs, n_epochs=100, patience=10)
+
+
+def experiment(algorithm, init_bc=False, discr_only_state=False):
+    from _wrapping_envs.PlottingEnv import PlottingEnv
+    mdp = PlottingEnv(env_class=Gym, env_kwargs=dict(name='Pendulum-v0',
+                                                     horizon=200, gamma=0.99))
+
+    if algorithm == "GAIL":
+        agent = _create_gail_agent(mdp, discr_only_state)
+    elif algorithm == "VAIL":
+        # train new agent with gail, following expert trajectories
+        agent = _create_vail_agent(mdp, discr_only_state)
+    else:
+        raise NotImplementedError
+    # normalization callback
+    normalizer = NormalizationBoxedPreprocessor(mdp_info=mdp.info)
+
+    # Algorithm(with normalizing and plotting)
+    core = Core(agent, mdp, preprocessors=[normalizer])
+    #core = Core(agent, mdp)    # without normalization
+
+    # evaluate untrained policy
+    dataset = core.evaluate(n_episodes=10)
+    J_mean = np.mean(compute_J(dataset, mdp.info.gamma))
+    print('Before Gail -> J: {}, Entropy: {}'.format(J_mean, agent.policy.entropy()))
+
+    if init_bc:
+        init_policy_with_bc(agent, normalizer=normalizer)
+        # evaluate untrained policy
+        dataset = core.evaluate(n_episodes=10)
+        J_mean = np.mean(compute_J(dataset, mdp.info.gamma))
+        print('After BC -> J: {}, Entropy: {}'.format(J_mean, agent.policy.entropy()))
+
+
+    epoch_js = []
+    # gail train loop
+    for it in range(100):
+        core.learn(n_steps=10000, n_steps_per_fit=1024, render=False)
+        dataset = core.evaluate(n_episodes=10, render=False)
+        J_mean = np.mean(compute_J(dataset, mdp.info.gamma))
+        epoch_js.append(J_mean)
+        print('Epoch: {}  ->  J: {}, Entropy: {}'.format(str(it), J_mean, agent.policy.entropy()))
+
+
+    print("--- The train has finished ---")
+    import matplotlib.pyplot as plt
+    plt.plot(epoch_js)
+    plt.show()
+
+    input()
+
+    # evaluate trained policy
+    dataset = core.evaluate(n_episodes=25)
+    J_mean = np.mean(compute_J(dataset, mdp.info.gamma))
+    print('After Gail -> J: {}, Entropy: {}'.format(J_mean, agent.policy.entropy()))
+
+
+if __name__ == "__main__":
+    algorithm = ["GAIL", "VAIL"]
+    experiment(algorithm=algorithm[1], init_bc=True, discr_only_state=True)
